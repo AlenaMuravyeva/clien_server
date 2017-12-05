1228a56459bc31d510bf6ac41ae53ee5e3a954b2
Alena Muravyeva <almurav@mera.ru>
2017-12-05 16:14:03 +0300

    move logger to separate file

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 import pars_cmd_for_client_server
 import signal
 import sys
-import logging
+import log
 
 
 def handler(signum, frame):
@@ -13,7 +13,7 @@ def handler(signum, frame):
 
 class Server():
     def __init__(self, port, address):
-        logger.info('creating an instance of Server')
+        log.logger.info('creating an instance of Server')
         self.max_clients = 1
         self.server_listen_port = port
         self.server_listen_ip = str(address)
@@ -23,18 +23,18 @@ class Server():
     def init_server(self):
         self.server_sock.bind((self.server_listen_ip, self.server_listen_port))
         self.server_sock.listen(self.max_clients)
-        logger.info("init_server: port {}, address {}".format(
+        log.logger.info("init_server: port {}, address {}".format(
             self.server_listen_port, self.server_listen_ip)
         )
 
     def close_socket(self):
         self.server_sock.close()
-        logger.info('Close server socket')
+        log.logger.info('Close server socket')
 
     def run_server(self):
         try:
             self.client_sock, self.client_addr = self.server_sock.accept()
-            logger.info(
+            log.logger.info(
                 "run_server client_sock {}, client_address{}".format(
                     self.client_sock, self.client_addr
                 )
@@ -43,20 +43,12 @@ class Server():
                 client_data = self.client_sock.recv(1024)
                 if client_data:
                     self.client_sock.send(client_data)
-                    logger.info('Send data {}'.format(client_data))
+                    log.logger.info('Send data {}'.format(client_data))
         finally:
             self.close_socket()
 
 
 if __name__ == '__main__':
-    logger = logging.getLogger("loger")
-    logger.setLevel(logging.INFO)
-    fh = logging.FileHandler("logger1.log")
-    formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    )
-    fh.setFormatter(formatter)
-    logger.addHandler(fh)
     port_ent_usr, address = pars_cmd_for_client_server.pars_cmd()
     server = Server(port_ent_usr, address)
     server.init_server()
